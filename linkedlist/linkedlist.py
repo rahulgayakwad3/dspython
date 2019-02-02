359853a0d2cd468e240b5d70497984f16b953696
Rahul Gayakwad <rahul.gayakwad@kuliza.com>
2019-02-03 02:32:22 +0530

    added comments to the linked list

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -10,33 +10,66 @@ class LinkedList(object):
     def __init__(self):
         self.head = None
 
+    def is_empty(self):
+        """
+        :return: True/False if a list is empty or not
+        """
+        return self.head is None
+
     def traverse(self):
+        """
+        print all elements of a linked list
+        """
         p = self.head
         while p is not None:
             print(p.data)
             p = p.next
 
     def insert_front(self, data):
+        """
+        inserts data in front of linked list
+        :param data: data to be inserted
+        """
         node = ListNode(data)
-        node.next = self.head
+        if self.head:
+            node.next = self.head
         self.head = node
 
     def insert_end(self, data):
+        """
+        inserts data in the end of linked list
+        :param data: data to be inserted
+        :return:
+        """
         node = ListNode(data)
-        p = self.head
-        while p.next is not None:
-            p = p.next
-        p.next = node
+        if self.head:
+            p = self.head
+            while p.next is not None:
+                p = p.next
+            p.next = node
+        else:
+            self.head = node
 
     def find_node(self, data):
+        """
+        finds the node with given data
+        :param data: data to be found
+        :return:
+        """
         p = self.head
         while p is not None:
-            if p.data == data
+            if p.data == data:
                 return p
             p = p.next
         return None
 
-    def insert_after(self, key, data):
+    def insert_after_node(self, key, data):
+        """
+        inserts data after a specific node
+        :param key: data of the node after which we want to insert a new node
+        :param data: data to be inserted
+        :return:
+        """
         node = ListNode(data)
         p = self.head
         while p is not None:
@@ -46,6 +79,12 @@ class LinkedList(object):
             p = p.next
 
     def insert_before(self, key, data):
+        """
+        insert before a specific key
+        :param key: key before which insertion has to be made
+        :param data: data to be inserted
+        :return:
+        """
         node = ListNode(data)
         p = self.head
         while p.next is not None:
@@ -55,7 +94,14 @@ class LinkedList(object):
             p = p.next
 
     def remove_node(self, data):
+        """
+        remove a specific node
+        :param data:
+        :return:
+        """
         p = self.head
         while p.next is not None:
             if p.next.data == data:
+                temp = p.next
                 p.next = p.next.next
+                del temp
